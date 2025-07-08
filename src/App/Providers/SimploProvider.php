c1fac63aa3802c82af740f10df58aa480b820891
Marcin Ulezalka <36479486+marcinulezalka@users.noreply.github.com>
2025-07-08 07:51:03 +0200

    Dodanie composer i providera

---
+++ src/App/Providers/SimploProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Simplysmart\Simplo\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+
+class SimploProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @param Router $router
+     * @return void
+     */
+    public function boot(Router $router): void
+    {
+
+    }
+}
