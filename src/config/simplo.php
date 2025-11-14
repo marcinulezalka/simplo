0e3216b828f2ab4757113c973d705f6a2c185d2a
Marcin Ulezalka <36479486+marcinulezalka@users.noreply.github.com>
2025-11-14 20:10:00 +0100

    Dodanie config

---
+++ src/config/simplo.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright (c) 2014-2025.
+ */
+
+/**
+ * ============================================================================
+ * Konfiguracja Modułu: Simplo
+ * ============================================================================
+ */
+
+return [
+    /**
+     * Ścieżka do szablonu modelu Eloquent.
+     */
+    'model_template_path' => base_path('vendor/simplysmart/simplo/src/resources/templates/model.php.stub'),
+
+    /**
+     * Pełna nazwa pakietu (np. vendor/nazwa).
+     * Używana do:
+     * - Identyfikacji zasobów (Assets, Seederów)
+     * - Ścieżek do storage i publikowanych zasobów
+     */
+    'package_name' => 'marcinulezalka/simplo',
+
+    /**
+     * Nazwa modułu (biznesowa).
+     * Może być używana w interfejsie, brandingach, nagłówkach.
+     */
+    'module_name' => 'Simplo',
+
+    /**
+     * Adres repozytorium modułu (opcjonalnie).
+     */
+    'module_repository_url' => '#',
+
+    /**
+     * Wersja modułu.
+     */
+    'module_version' => '1.0',
+
+    /**
+     * Lista pól do wykluczenia z generowanych tablic fillable, hidden, casts, attributes.
+     */
+    'exclude' => [
+        'fillable'   => ['created_at', 'updated_at', 'deleted_at'],
+        'casts'      => ['created_at', 'updated_at', 'deleted_at'],
+        'attributes' => ['created_at', 'updated_at', 'deleted_at'],
+        'hidden'     => [],
+    ],
+
+
+];
